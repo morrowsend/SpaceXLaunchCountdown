b8dafff99de99e77285ce1d51b3d3fbc2fb43546
morrowsend <adam.sheekgeek@gmail.com>
2022-06-05 16:51:12 -0400

    Working multiple days

--- parseRocketLaunches.py
+++ parseRocketLaunches.py
@@ -86,30 +86,43 @@
 
 
 
-
 ##########################################################################
-# #How to make python run the "getLaunchList" function every day at 1am: https://stackoverflow.com/a/15090893
-# from datetime import datetime, timedelta
-# from threading import Timer
-# from datetime import datetime, time, timedelta
-# from threading import Timer
-# from time import sleep
+#How to make python run the "getLaunchList" function every day at 1am: https://stackoverflow.com/a/15090893
+from datetime import datetime, timedelta
+from threading import Timer
+from datetime import datetime, time, timedelta
+from threading import Timer
+from time import sleep
 
 
-# def hello_world():
-#     print ("hello world")
-#     #...
+def hello_world():
+    print ("hello world")
+    #...
 
-# x=datetime.today()
 
-# y = x.replace(day=x.day, hour=15, minute=54, second=0, microsecond=0) + timedelta(days=0) #at 1:00am tomorrow...
-# delta_t=y-x #get the difference from now until the next time we want to run the check of launches
 
-# t = Timer(delta_t.total_seconds(), hello_world) 
-# t.start()
-# print("Still working")
-# sleep(1)
-# print("still...")
+
+
+#This function is used for testing so you don't overrun the rocket server
+def FAKEgetLaunchList():
+    print("Fake Launch time")
+    launchDate = secondDate  # <-- change this date to a future date for testing
+    return (launchDate)
+
+
+
+x=datetime.today()
+
+y = x.replace(day=x.day, hour=x.hour, minute=x.minute)+ timedelta(minutes=1) #at 1:00am tomorrow...
+delta_t=y-x #get the difference from now until the next time we want to run the check of launches
+
+print("going off in "+str(y))
+
+t = Timer(delta_t.total_seconds(), hello_world) 
+t.start()
+print("Still working")
+sleep(1)
+print("still...")
 
 
 ##########################################################################
